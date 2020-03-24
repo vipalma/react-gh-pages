c47df5c51045cb1a6c94c0962d36c1dbd23ad5da
vipalma <vipalma@gmail.com>
2020-03-24 18:02:04 +0100

    Updates

---
+++ precache-manifest.3f8cf7c7823392ded1a1f7f3ed07206e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d447eba3dc94bb8f7f5d6a8fe6cb3f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c0e4f25982cfd30e34d7",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5c39051751acff5b36b",
+    "url": "/react-gh-pages/static/js/2.3ee159c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.3ee159c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e4f25982cfd30e34d7",
+    "url": "/react-gh-pages/static/js/main.1144896b.chunk.js"
+  },
+  {
+    "revision": "c910589a90c7db292e97",
+    "url": "/react-gh-pages/static/js/runtime-main.8d2d02b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
